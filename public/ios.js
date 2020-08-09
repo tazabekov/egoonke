4acb884cb8d53cb012ebbbaa85ba78e64e7b2fc0
Gizzat <gizzat.tazabekov@gmail.com>
2020-08-08 18:37:52 -0700

    ios test

---
+++ public/ios.js
@@ -0,0 +1,17 @@
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+if (navigator.mediaDevices) {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+
+    myVideo.srcObject = stream;
+    myVideo.addEventListener('loadedmetadata', () => {
+      myVideo.play();
+    });
+    videoGrid.append(myVideo);
+  });
+}
